ea92002500a036e351acbab8ae61c7da536c4ce3
Huraira Younas <75196437+MianHuraira@users.noreply.github.com>
2023-12-06 22:17:26 +0500

    map

--- src/components/Modal/LocationMap.jsx
+++ src/components/Modal/LocationMap.jsx
@@ -1,79 +1,79 @@
-import React, { useRef, useState, useEffect } from "react";
-import arrowLeft from "../../assets/logo/icons/arrow_left.svg";
-import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
+// import React, { useRef, useState, useEffect } from "react";
+// import arrowLeft from "../../assets/logo/icons/arrow_left.svg";
+// import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
 
-const LocationMap = ({ setIsMapVisible }) => {
-  const mapRef = useRef(null);
-  const [selectedLocation, setSelectedLocation] = useState(null);
+// const LocationMap = ({ setIsMapVisible }) => {
+//   const mapRef = useRef(null);
+//   const [selectedLocation, setSelectedLocation] = useState(null);
 
-  const handleMapClick = (mapProps, map, clickEvent) => {
-    const lat = clickEvent.latLng.lat();
-    const lng = clickEvent.latLng.lng();
-    setSelectedLocation({ lat, lng });
-  };
+//   const handleMapClick = (mapProps, map, clickEvent) => {
+//     const lat = clickEvent.latLng.lat();
+//     const lng = clickEvent.latLng.lng();
+//     setSelectedLocation({ lat, lng });
+//   };
 
-  const handleSaveClick = () => {
-    console.log("Selected Location:", selectedLocation);
-    setIsMapVisible(false);
-  };
+//   const handleSaveClick = () => {
+//     console.log("Selected Location:", selectedLocation);
+//     setIsMapVisible(false);
+//   };
 
-  useEffect(() => {
-    // Fetch the user's current location using Geolocation API
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          const initialLocation = { lat: latitude, lng: longitude };
-          mapRef.current.map.setCenter(initialLocation);
-          setSelectedLocation(initialLocation);
-        },
-        (error) => {
-          console.error("Error getting current location:", error);
-        }
-      );
-    } else {
-      console.error("Geolocation is not supported by this browser.");
-    }
-  }, []);
+//   useEffect(() => {
+//     // Fetch the user's current location using Geolocation API
+//     if (navigator.geolocation) {
+//       navigator.geolocation.getCurrentPosition(
+//         (position) => {
+//           const { latitude, longitude } = position.coords;
+//           const initialLocation = { lat: latitude, lng: longitude };
+//           mapRef.current.map.setCenter(initialLocation);
+//           setSelectedLocation(initialLocation);
+//         },
+//         (error) => {
+//           console.error("Error getting current location:", error);
+//         }
+//       );
+//     } else {
+//       console.error("Geolocation is not supported by this browser.");
+//     }
+//   }, []);
 
-  return (
-    <>
-      <section
-        className={`p-3 ${
-          selectedLocation ? "map-container" : "map-container-empty"
-        }`}
-      >
-        <img
-          src={arrowLeft}
-          onClick={() => setIsMapVisible(false)}
-          className="cursorP"
-          alt=""
-        />
-        <div className="position-relative h-75 d-flex flex-column justify-content-between">
-          <Map
-            ref={mapRef}
-            google={window.google}
-            zoom={14}
-            onClick={handleMapClick}
-          >
-            {selectedLocation && (
-              <Marker
-                position={{
-                  lat: selectedLocation.lat,
-                  lng: selectedLocation.lng,
-                }}
-              />
-            )}
-          </Map>
-        </div>
-        <button className="btn-primary text-center" onClick={handleSaveClick}>
-          Save
-        </button>
-      </section>
-    </>
-  );
-};
+//   return (
+//     <>
+//       <section
+//         className={`p-3 ${
+//           selectedLocation ? "map-container" : "map-container-empty"
+//         }`}
+//       >
+//         <img
+//           src={arrowLeft}
+//           onClick={() => setIsMapVisible(false)}
+//           className="cursorP"
+//           alt=""
+//         />
+//         <div className="position-relative h-75 d-flex flex-column justify-content-between">
+//           <Map
+//             ref={mapRef}
+//             google={window.google}
+//             zoom={14}
+//             onClick={handleMapClick}
+//           >
+//             {selectedLocation && (
+//               <Marker
+//                 position={{
+//                   lat: selectedLocation.lat,
+//                   lng: selectedLocation.lng,
+//                 }}
+//               />
+//             )}
+//           </Map>
+//         </div>
+//         <button className="btn-primary text-center" onClick={handleSaveClick}>
+//           Save
+//         </button>
+//       </section>
+//     </>
+//   );
+// };
 
-export default GoogleApiWrapper({
-  apiKey: "AIzaSyBH0Ey-G2PbWkSCLyGG1A9TCg9LDPlzQpc",
-})(LocationMap);
+// export default GoogleApiWrapper({
+//   apiKey: "AIzaSyBH0Ey-G2PbWkSCLyGG1A9TCg9LDPlzQpc",
+// })(LocationMap);
